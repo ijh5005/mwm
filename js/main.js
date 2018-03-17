21b5a244b499b432a92d8525e15a85c1cd50c0be
Harrison <Isaiah_Harrison@comcast.com>
2018-03-17 18:38:23 -0400

    - navigation refactor - contact page added

--- js/main.js
+++ js/main.js
@@ -31,8 +31,8 @@ app.controller('ctrl', ['$rootScope', '$scope', '$interval', '$timeout', 'animat
   $rootScope.featureImg = './img/featureImg.png';
   //current page
   $rootScope.currentPage = 'home';
-  //underlines the specified page in the naigation with the active class
-  $scope.nav = { home: 'active', about: '', artist: '', staff: '', contact: '' };
+  //navigation options
+  $rootScope.navigationOptions = data.navigationOptions;
   $scope.toggleMusic = () => {
     //restart song if at the end of the song
     ($rootScope.musicCurrentTime === $rootScope.musicFinishTime) ? $rootScope.musicCurrentTime = 0 : null;
@@ -54,15 +54,14 @@ app.controller('ctrl', ['$rootScope', '$scope', '$interval', '$timeout', 'animat
     task.scrollSongInPlace(data);
   }
   //navigate website
-  $scope.navigateTo = (page) => {
+  $scope.navigateTo = (page, hasInitiallyLoaded) => {
     //return null if already on page
-    if(page === $rootScope.currentPage){ return null }
+    if((page === $rootScope.currentPage) && !hasInitiallyLoaded){ return null }
+    //fade the logo opacity on different pages for contrast
+    const opacity = ((page === 'artist') || (page === 'services') || (page === 'contact')) ? 0 : 0.1;
+    $('#logo img').css('opacity', opacity);
     //hide the navigation until done transitioning pages
     $('#navigation').fadeOut(500);
-    //clear the active page underline from all navigation options
-    $scope.nav = { home: '', about: '', artist: '', staff: '', contact: '' };
-    //add the active underline to the clicked on navigation
-    $scope.nav[page] = 'active';
     //transition from the current page
     animation.navigationFrom(data.navigationAnimations[$rootScope.currentPage]);
     //transition to the next page
@@ -71,9 +70,19 @@ app.controller('ctrl', ['$rootScope', '$scope', '$interval', '$timeout', 'animat
     if(page === 'artist'){
       $timeout(() => { $scope.moveSlider(6); }, 1000)
     }
+    //wait for page to refresh to add the active class to the navigation page
+    $timeout(() => {
+      //clear the active page class from all navigation options
+      $('.navigationOption').removeClass('active');
+      //current page index
+      const index = task.findIndexOfPageByName(page, data.navigationOptions);
+      //add the active underline to the clicked on navigation
+      $('.navigationOption[data="' + index + '"]').addClass('active');
+    })
   }
-  task.hidePages();
-
+  task.hidePages(data.navigationOptions);
+  //services
+  $scope.services = data.services;
   //slideshow methods
   $scope.artists = data.artists;
   $scope.currentArtist = data.artists[6];
@@ -82,7 +91,10 @@ app.controller('ctrl', ['$rootScope', '$scope', '$interval', '$timeout', 'animat
     task.slideItem(index);
   }
   $timeout(() => {
+    const hasInitiallyLoaded = true;
+    $scope.navigateTo('home', hasInitiallyLoaded);
     task.preScrollSlider();
+    $('#navigation').removeClass('none');
   })
   task.setArtistImgs(data.artists);
 }]);
@@ -139,8 +151,8 @@ app.service('animation', function($rootScope, $interval, $timeout, data, task){
           const $selector = $(animation['selector']);
           const animationClass = animation['animation'];
           $selector.removeClass(animationClass);
-          $('#navigation').fadeIn(500);
         })
+        $('#navigation').fadeIn(500);
       }, 100)
     }, 800)
   }
@@ -225,13 +237,21 @@ app.service('data', function(){
       { selector: '#staffPageTopBottom', animation: 'topStaffPage'},
       { selector: '#staffPage', animation: 'fade'}
     ],
-    contact: []
+    services: [
+      { selector: '#servicesPageLeftSide.servicesPageLeftSide', animation: 'fade'},
+      { selector: '#servicesPageRightSide.servicesPageRightSide', animation: 'fade'}
+    ],
+    contact: [
+      { selector: '#contactPageLeftSide.contactPageLeftSide', animation: 'fade'},
+      { selector: '#contactPageRightSide.contactPageRightSide', animation: 'fade'}
+    ]
   }
   this.artists = [
     {
+      sign: '<<',
       name: '',
       img: '',
-      bio: 'Lorem Ipsum ist ein einfacher Demo-Text für die Print- und Schriftindustrie. Lorem Ipsum ist in der Industrie bereits der Standard Demo-Text seit 1500, als ein unbekannter Schriftsteller eine Hand voll Wörter nahm und.'
+      bio: ''
     },
     {
       name: 'Leon "Pop Traxx" Huff, Jr.',
@@ -279,9 +299,24 @@ app.service('data', function(){
       bio: 'Lorem Ipsum ist ein einfacher Demo-Text für die Print- und Schriftindustrie. Lorem Ipsum ist in der Industrie bereits der Standard Demo-Text seit 1500, als ein unbekannter Schriftsteller eine Hand voll Wörter nahm und.'
     },
     {
+      sign: '>>',
       name: '',
       img: ''
     }
+  ]
+  this.navigationOptions = [
+    {index: 0, selector: '#homePage', name: 'HOME'},
+    {index: 1, selector: '#aboutPage', name: 'ABOUT'},
+    {index: 2, selector: '#artistPage', name: 'ARTIST'},
+    {index: 3, selector: '#servicesPage', name: 'SERVICES'},
+    {index: 4, selector: '#contactPage', name: 'CONTACT'}
+  ]
+  this.services = [
+    'Consulting',
+    'Studio Time',
+    'Post to Final Production',
+    'Publishing (Publish & Collect  Royalties for artist)',
+    'Original Music production'
   ]
 });
 
@@ -315,10 +350,12 @@ app.service('task', function($rootScope, $interval, $timeout){
     document.getElementById('playlistSectionHolder').scrollTop = scrollSongTo;
   }
   //hide all page except the homepage
-  this.hidePages = () => {
-    $('#aboutPage').addClass('none');
-    $('#artistPage').addClass('none');
-    $('#staffPage').addClass('none');
+  this.hidePages = (navigationOptions) => {
+    navigationOptions.map((data, index) => {
+      if(index > 0){
+        $(data.selector).addClass('none');
+      }
+    })
   }
   ////////slider methods
   this.preScrollSlider = () => {
@@ -346,5 +383,14 @@ app.service('task', function($rootScope, $interval, $timeout){
         $('.slideItems[data="' + index + '"]').css('backgroundImage', "url('" + data.img + "')");
       }, 1000)
     })
+  }
+  this.findIndexOfPageByName = (page, navigationOptions) => {
+    let index = null;
+    navigationOptions.map((data) => {
+      if(data.name.toLowerCase() === page){
+        index = data.index;
+      }
+    })
+    return index;
   }
 });
